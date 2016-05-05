9f4586c30b5d87c083a65e9c1a51b5e70ddaf713
Edersilvapaz <edersilvapaz@gmail.com>
2016-05-04 22:09:29 -0500

    asd

---
+++ Frogger/src/entities/Fly.java
@@ -0,0 +1,22 @@
+package entities;
+
+import java.awt.Graphics;
+
+import game.Game;
+
+public class Fly extends Entity{
+
+	public Fly(Game game, float x, float y) {
+		super(game, x, y,0,0,0);
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+	}
+}
